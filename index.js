fe4a3f184455784c01c8236712970d7973885ae2
saidsoud72 <said.omar@student.moringaschool.com>
2022-08-21 16:54:50 +0300

    will include controll flow lab

--- index.js
+++ index.js
@@ -9,3 +9,31 @@ function ternaryCheckCity(){
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+function scuberGreetingForFeet(someValue){
+  // Write your code here!
+  if(someValue<=400){
+    return "This one is on me!"
+  }
+  else if(someValue < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(someValue >2500){
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city){
+  return city ==='NYC'? 'Ok, sounds good.': 'No go.'
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+}
